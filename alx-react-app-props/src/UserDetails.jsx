848168a10a9ab48a2eb91a0372f4444f2359d188
Frimpong Michael <frimpstchalla@gmail.com>
2025-11-06 23:32:54 +0000

    feat: update to alx-react-app-props

--- alx-react-app-props/src/UserDetails.jsx
+++ alx-react-app-props/src/UserDetails.jsx
@@ -1,16 +1,16 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import UserContext from './UserContext';
 
 function UserDetails() {
 
     const userData = useContext(UserContext);
 
-  return (
-    <div>
-        <p>Name: {userData.name}</p>
-        <p>Email: {userData.email}</p>
-    </div>
-  );
+    return (
+        <div>
+            <p>Name: {userData.name}</p>
+            <p>Email: {userData.email}</p>
+        </div>
+    );
 }
 
 export default UserDetails;
