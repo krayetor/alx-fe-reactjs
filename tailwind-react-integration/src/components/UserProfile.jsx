ec4120f8374f4d78439d8ed3c3f50ebc22a54239
Frimpong Michael <frimpstchalla@gmail.com>
2025-11-28 12:40:15 +0000

    added hover animation

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 function UserProfile() {
     return (
-        <div className="bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm d mx-auto my-20 rounded-lg shadow-lg">
+        <div className="bg-gray-100 md:p-8 sm:p-4 max-w-xs md:max-w-sm d mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl">
             <img
-                className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+                className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
                 src="https://via.placeholder.com/150"
                 alt="User"
             />
-            <h1 className="text-lg md:text-xl text-blue-800 my-4">John Doe</h1>
+            <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
             <p className="text-gray-600 text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
         </div>
     );
