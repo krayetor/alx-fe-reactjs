b12309a9a1bb21cfcd499b70bc42a27d1f1b04a7
Frimpong Michael <frimpstchalla@gmail.com>
2025-12-03 22:07:01 +0000

    inititial commit

---
+++ recipe-sharing-platform/postcss.config.cjs
@@ -0,0 +1,9 @@
+const autoprefixer = require("autoprefixer");
+const { plugin } = require("postcss");
+
+module.export = {
+    plugin: {
+        tailwindcss: {},
+        autoprefixer: {},
+    },
+};
---
+++ recipe-sharing-platform/tailwind.config.cjs
@@ -0,0 +1,14 @@
+/* @type {import('tailwindcss').Config} */
+
+const { plugin } = require("postcss");
+
+module.exports = {
+    content: [
+        './index.html',
+        './src/**/*.{js,jsx,ts,tsx}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
